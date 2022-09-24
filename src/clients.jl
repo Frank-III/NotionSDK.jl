7f0b53460f8823ca6a62fc49ccbdb57817e96600
frankwang <73262844+Frank-III@users.noreply.github.com>
2022-09-24 12:30:26 +0800

    Update clients.jl

--- src/clients.jl
+++ src/clients.jl
@@ -1,26 +1,12 @@
 include("utils.jl")
 
-# switch to EasyConfig
-# @with_kw struct ClientOptions
-# 	#auth::String = nothing
-# 	#notion_version::String = "2022-06-28"
-# 	#conten_type::String = "application/json" # seems to be important
-# 	header::Config = Config(auth=nothing, notion_version="2022-06-28", content_type="application/json")
-# 	base_url::String = "https://api.notion.com/v1/"
-# 	timeout_ms::Int = 60_000
-# end
-
 # would probablity move to utils
 const HEADER_NAME = ["Authorization", "Notion-Version", "Content-Type"] # shoule we set HADER_NAME?
-
-#function _parse_header(headers::Config)
-#    return HEADER_NAME .=> collect(values(headers))
-#end
 
 abstract type BaseClient end
 
 struct Client <: BaseClient
-    header::Config
+	header::Config
 	base_url::String
 	timeout_ms::Int
 	#log_level::Int= nothing
@@ -43,8 +29,8 @@ end
 
 function make_request(client::BaseClient, method::String, path::String; body=Dict(), query=Dict(), auth=nothing)
 	try
-        response = _build_request(client, method, path; body=body, query=query, auth=auth)()
-        return _parse_response(response)
+		response = _build_request(client, method, path; body=body, query=query, auth=auth)()
+		return _parse_response(response)
 	catch e
 		return HTTP.Response(400, "Error: $e")
 	#end
@@ -61,8 +47,8 @@ end
 
 function list_users(notion::BaseClient; kwargs...)
 	make_request(notion, "GET", "users";
-        query= pick(kwargs, :start_cursor, :page_size),
-        auth= get_auth(kwargs))
+		query= pick(kwargs, :start_cursor, :page_size),
+		auth= get_auth(kwargs))
 end
 
 get_me(notion::BaseClient; kwargs...) = make_request(notion, "GET", "users/me"; auth=get_auth(kwargs))
@@ -71,54 +57,54 @@ get_me(notion::BaseClient; kwargs...) = make_request(notion, "GET", "users/me"; 
 ## Page Functions
 function create_page(notion::BaseClient; kwargs...)
 	make_request(notion, "POST", "pages";
-        body=pick(kwargs, :parent, :properties, :children, :icon, :cover),
-        auth=get_auth(kwargs))
+		body=pick(kwargs, :parent, :properties, :children, :icon, :cover),
+		auth=get_auth(kwargs))
 end
 
 retrieve_page(notion::BaseClient, page_id::String; kwargs...) = make_request(notion, "GET", "pages/$(page_id)"; auth=get_auth(kwargs))
 
 function update_page(notion::BaseClient, page_id::String; kwargs...)
 	make_request(notion, "PATCH", "pages/$(page_id)";
-        body=pick(kwargs, :achieved, :properties, :icon, :cover),
-        auth=get_auth(kwargs))
+	body=pick(kwargs, :achieved, :properties, :icon, :cover),
+	auth=get_auth(kwargs))
 end
 
 function retrieve_page_properties(notion::BaseClient, page_id::String, property_id::String; kwargs...)
 	make_request(notion, "GET", "pages/$(page_id)/properties/$(property_id)";
-        query=pick(kwargs, :start_cursor, :page_size),
-        auth=get_auth(kwargs))
+	query=pick(kwargs, :start_cursor, :page_size),
+	auth=get_auth(kwargs))
 end
 
 ## Database Functions
 function list_databases(notion::BaseClient; kwargs...)
-    notion.header.notion_version !== "2021-08-16" && error("this function is deprecated")
+	notion.header.notion_version !== "2021-08-16" && error("this function is deprecated")
 	make_request(notion, "GET", "databases";
-        query=pick(kwargs, :start_cursor, :page_size),
-        auth=get_auth(kwargs))
+		query=pick(kwargs, :start_cursor, :page_size),
+		auth=get_auth(kwargs))
 end
 
 function query_databases(notion::BaseClient, database_id::String; kwargs...)
 	make_request(notion, "POST", "databases/$(database_id)/query";
-        body=pick(kwargs, :filter, :sorts, :start_cursor, :page_size),
-        auth=get_auth(kwargs))
+		body=pick(kwargs, :filter, :sorts, :start_cursor, :page_size),
+		auth=get_auth(kwargs))
 end
 
 function retrieve_databases(notion::BaseClient, database_id::String; kwargs...)
 	make_request(notion, "POST", "databases/$(database_id)/query";
-        body=pick(kwargs, :filter, :sorts, :start_cursor, :page_size),
-        auth=get_auth(kwargs))
+		body=pick(kwargs, :filter, :sorts, :start_cursor, :page_size),
+		auth=get_auth(kwargs))
 end
 
 function create_databases(notion::BaseClient; kwargs...)
 	make_request(notion, "POST", "databases/$(database_id)";
-        body=pick(kwargs, :parent, :title, :properties, :icon, :cover),
-        auth=get_auth(kwargs))
+		body=pick(kwargs, :parent, :title, :properties, :icon, :cover),
+		auth=get_auth(kwargs))
 end
 
 function update_databases(notion::BaseClient, database_id::String; kwargs...)
 	make_request(notion, "PATCH", "databases/$(database_id)";
-        body=pick(kwargs, :achieved, :properties, :icon, :cover),
-        auth=get_auth(kwargs))
+		body=pick(kwargs, :achieved, :properties, :icon, :cover),
+		auth=get_auth(kwargs))
 end
 
 
@@ -127,50 +113,50 @@ retrieve_block(notion::BaseClient, block_id::String; kwargs...) = make_request(n
 
 function update_block(notion::BaseClient, block_id::String; kwargs...)
 	make_request(notion, "PATCH", "blocks/$(block_id)";
-        body=pick(kwargs,
-        :embed,
-        :type,
-        :archived,
-        :bookmark,
-        :image,
-        :video,
-        :pdf,
-        :file,
-        :audio,
-        :code,
-        :equation,
-        :divider,
-        :breadcrumb,
-        :table_of_contents,
-        :link_to_page,
-        :table_row,
-        :heading_1,
-        :heading_2,
-        :heading_3,
-        :paragraph,
-        :bulleted_list_item,
-        :numbered_list_item,
-        :quote,
-        :to_do,
-        :toggle,
-        :template,
-        :callout,
-        :synced_block,
-        :table
-        ),
-        auth=get_auth(kwargs))
+		body=pick(kwargs,
+		:embed,
+		:type,
+		:archived,
+		:bookmark,
+		:image,
+		:video,
+		:pdf,
+		:file,
+		:audio,
+		:code,
+		:equation,
+		:divider,
+		:breadcrumb,
+		:table_of_contents,
+		:link_to_page,
+		:table_row,
+		:heading_1,
+		:heading_2,
+		:heading_3,
+		:paragraph,
+		:bulleted_list_item,
+		:numbered_list_item,
+		:quote,
+		:to_do,
+		:toggle,
+		:template,
+		:callout,
+		:synced_block,
+		:table
+		),
+		auth=get_auth(kwargs))
 end
 
 
 ## Block children functions
 function append_block_children(notion::BaseClient, block_id::String; kwargs...)
-    make_request(notion, "PATCH", "blocks/$(block_id)/children";
+	make_request(notion, "PATCH", "blocks/$(block_id)/children";
 		body=pick(kwargs, :children),
 		auth=get_auth(kwargs))
 end
 
 function list_block_children(notion::BaseClient, block_id::String; kwargs...)
-    make_request(notion, "GET", "blocks/$(block_id)/children";
+	make_request(notion, "GET", "blocks/$(block_id)/children";
 		query=pick(kwargs, :start_cursor, :page_size),
 		auth=get_auth(kwargs))
 end
@@ -178,26 +164,23 @@ end
 ## Search function
 
 function search(notion::BaseClient, block_id::String; kwargs...)
-    make_request(notion, "POST", "search";
-        query=pick(kwargs, :query, :sort, :filter, :start_cursor, :page_size),
-        auth=get_auth(kwargs))
+	make_request(notion, "POST", "search";
+		query=pick(kwargs, :query, :sort, :filter, :start_cursor, :page_size),
+		auth=get_auth(kwargs))
 end
 
 ## Comment function
 function create_comment(notion::BaseClient; kwargs...)
-    make_request(notion, "POST", "comments";
-        query=pick(kwargs, :parent, :discussion_id, :rich_text),
-        auth=get_auth(kwargs))
+	make_request(notion, "POST", "comments";
+		query=pick(kwargs, :parent, :discussion_id, :rich_text),
+		auth=get_auth(kwargs))
 end
 
 function list_comments(notion::BaseClient; kwargs...)
-    make_request(notion, "GET", "comments";
-    query=pick(kwargs, :block_id, :start_cursor, :page_size),
-    auth=get_auth(kwargs))
+	make_request(notion, "GET", "comments";
+		query=pick(kwargs, :block_id, :start_cursor, :page_size),
+		auth=get_auth(kwargs))
 end
-
-
-
 
 
 
