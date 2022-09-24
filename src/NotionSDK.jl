fd8156bc7e0d7111bee26f69e4c1a603914d3f81
frankwang <73262844+Frank-III@users.noreply.github.com>
2022-09-24 12:24:40 +0800

    Update NotionSDK.jl

--- src/NotionSDK.jl
+++ src/NotionSDK.jl
@@ -9,7 +9,7 @@ using HTTP
 include("clients.jl")
 
 export
-    Client, AsyncClient,
+   	Client, AsyncClient,
 	retrieve_user, list_users, get_me,
 	create_page, retrieve_page, retrieve_page_properties, update_page,
 	list_databases, query_databases, retrieve_databases, create_databases,update_databases,
@@ -17,6 +17,4 @@ export
     list_block_children, append_block_children,
     search,
     create_comment, list_comments
-
-
 end # module NotionSDK
